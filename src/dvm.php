0105490cca70805b12ed3b5b5f9467a7f4b6420e
Brian Wood <bwood@berkeley.edu>
2016-01-22 17:05:21 -0800

    WIP: Add all values needed for new site.

--- src/dvm.php
+++ src/dvm.php
@@ -14,6 +14,7 @@ if ($return != 0) {
 $env = array(
   'DVM_TERMINUS',  // path to terminus
   'DVM_PROJ_DIR',  // path to projects
+  'DVM_DVM_DIR'    // path to drupal-vm installation
 );
 
 $needed_vars = array();
@@ -48,6 +49,7 @@ if (!is_dir($config_dir) || !is_writable($config_dir)) {
 
 
 // Load existing drupalvm config
+//TODO: if file doesn't exist in ~/.dvm, create from template
 $file = __DIR__ . "/../plugins/drupalvm_config.yml";
 $yaml = Yaml::parse(file_get_contents($file));
 
@@ -66,13 +68,15 @@ if (!terminus_site_info($site_name)) {
 }
 
 $site_dir = $dvm_proj_dir . '/' . $site_name;
+$site_dir_vm = '/var/www/' . $site_dir;
 if (!is_dir($site_dir)) {
   if (file_exists($site_dir)) {
     print "Error: $site_dir appears to be a plain file. Please move or remove it.\n";
     exit(1);
   }
-
   //TODO: clone the site
+  print "clone site not implemented.\n";
+  exit(1);
 }
 else {
   if (!is_writable($site_dir)) {
@@ -84,23 +88,50 @@ else {
     exit(1);
   }
 
-  print "Using git to pull the latest code into $site_dir.\n";
+  print "Using git to pull the latest code into $site_dir...\n";
   $cmd = "cd $site_dir;git pull";
-  exec($cmd, $output, $return);
-  if ($return != 0) {
-    print "Error: Could not run '$cmd'\n";
-    print implode("\n", $output);
-  }
-
+  //TODO: enable
+  /***********
+  * exec($cmd, $output, $return);
+  * if ($return != 0) {
+    * print "Error: Could not run '$cmd'\n";
+    * print implode("\n", $output);
+   * }
+   *********/
 }
 
-$yaml['vagrant_synced_folders'][]['local_path'] = $site_dir;
-$yaml['vagrant_synced_folders'][]['destination'] = '/var/www/' . $site_name;
-$yaml['vagrant_synced_folders'][]['type'] = 'nfs';
-$yaml['vagrant_synced_folders'][]['create'] = 1;
+//TODO: test that these won't be added multiple times
+if (!array_search($site_dir, array_column($yaml['vagrant_synced_folders'], 'local_path'))) {
+  $yaml['vagrant_synced_folders'][] = array(
+    'local_path' => $site_dir,
+    'destination' => $site_dir_vm,
+    'type' => 'nfs',
+    'create' => 1
+  );
+}
 
+if (!array_search($site_dir_vm, array_column($yaml['apache_vhosts'], 'documentroot'))) {
+  $yaml['apache_vhosts'][] = array(
+    'servername' => "$site_name.localhost",
+    'documentroot' => $site_dir_vm,
+    'extra_parameters' => "ProxyPassMatch ^/(.*\.php(/.*)?)$ \"fcgi://127.0.0.1:9000$site_dir_vm\"",
+  );
+}
+
+if (!array_search($site_name, array_column($yaml['mysql_databases'], 'name'))) {
+  $yaml['mysql_databases'][] = array(
+    'name' => $site_name,
+    'encoding' => "utf8",
+    'collation' => "utf8_general_ci"
+  );
+}
 // Write config.yml for drupalvm
 
 $dumper = new Symfony\Component\Yaml\Dumper();
-$yaml = $dumper->dump($yaml, 2);
-print $yaml . "\n";
+$yaml_dumped = $dumper->dump($yaml, 2);
+print $yaml_dumped . "\n";
+
+//backup
+//TODO: commit to git?
+rename("$dvm_dvm_dir/config.yml", "$dvm_dvm_dir/config.yml-" . time());
+file_put_contents("$dvm_dvm_dir/config.yml", $yaml_dumped);
